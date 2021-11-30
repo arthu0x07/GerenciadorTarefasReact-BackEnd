8017ef72ae9f614792713e68e0e7925683bff4f1
Arthur Santos <arthur_bsggg@hotmail.com>
2021-11-30 15:22:03 -0300

    Refeito

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 
 const servidor = express();
 
 servidor.use(express.json());
+servidor.use(cors())
 
-const TaskRouters = require('./routes/TaskRoute');
+const TaskRouters = require('./routers/TaskRoute');
 
 servidor.use('/task', TaskRouters);
 
-servidor.listen(7000)
+servidor.listen(3333)
